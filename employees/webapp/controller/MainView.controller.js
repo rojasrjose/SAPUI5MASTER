326eab7449b1eaf590efb5d89590c3f3377f990e
rojasrjose <joserojas.2@gmail.com>
2022-04-20 00:01:06 +0000

    Commit Elemntos Ctrl

--- employees/webapp/controller/MainView.controller.js
+++ employees/webapp/controller/MainView.controller.js
@@ -7,9 +7,26 @@ sap.ui.define([
     function (Controller) {
         "use strict";
 
-        return Controller.extend("logaligroup.employees.controller.MainView", {
-            onInit: function () {
+        function myCheck() {
+            var inputEmployee = this.byId("inputEmployee");
+            var valueEmployee = inputEmployee.getValue();
+            const oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
 
-            }
+            if (valueEmployee.length === 6) {
+               // inputEmployee.setDescription(oResourceBundle.getText("textOk"));
+               this.getView().byId("labelCountry").setVisible(true);
+               this.getView().byId("slCountry").setVisible(true);
+            } else {
+               // inputEmployee.setDescription(oResourceBundle.getText("textNotok"));    
+               this.getView().byId("labelCountry").setVisible(false);
+               this.getView().byId("slCountry").setVisible(false);          
+            };
+        }
+        return Controller.extend("logaligroup.employees.controller.MainView", {
+
+            onInit: function () {
+            },
+
+            onValidate: myCheck
         });
     });
